12b2c9ad2d775fc43bbdd7b0d4f18dbc786bd18f
Juli <juli.salazar18@hotmail.com>
2020-02-23 20:08:43 -0500

    Javascript added

---
+++ script/main.js
@@ -0,0 +1,12 @@
+/*var search = document.querySelector('input');
+var handleSearchKeyDown = function (hola){
+    if (hola.keyCode == 13){
+
+        console.log(search.value);
+        search.value = "";
+    }
+}
+search.addEventListener('keydown', handleSearchKeyDown);*/
+var iconMenu = document.querySelector("icon-menu");
+
+
