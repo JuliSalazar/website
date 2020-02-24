c6c8a9d3c49713b4c249f552f7b3b8fc7a64b3c6
Julieth <juli.salazar18@hotmail.com>
2020-02-23 21:48:16 -0500

    New sections added (gallery, newsletter, ad) and aptitudes section modified

--- script/main.js
+++ script/main.js
@@ -7,6 +7,21 @@ var handleSearchKeyDown = function (hola){
     }
 }
 search.addEventListener('keydown', handleSearchKeyDown);*/
-var iconMenu = document.querySelector("icon-menu");
+var headerNav = document.querySelector('.header__nav');
+var iconMenu = document.querySelector('.icon-menu');
+var handleMenuClick = function () {
+    
+    headerNav.classList.toggle('header__nav--mobile');
+}
+iconMenu.addEventListener('click', handleMenuClick);
+
+function handleWindowResize () {
+    if(window.innerWidth > 400){
+        headerNav.classList.remove('header__nav--mobile');
+    }
+}
+window.addEventListener('resize', handleWindowResize);
+
+
 
 
