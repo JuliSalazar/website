b507b6b63318f8c5e12b74450de8a4770af7036a
Juli <juli.salazar18@hotmail.com>
2020-03-04 08:27:14 -0500

    Gallery slider functioning

--- script/main.js
+++ script/main.js
@@ -1,12 +1,4 @@
-/*var search = document.querySelector('input');
-var handleSearchKeyDown = function (hola){
-    if (hola.keyCode == 13){
-
-        console.log(search.value);
-        search.value = "";
-    }
-}
-search.addEventListener('keydown', handleSearchKeyDown);*/
+//Javascript para funcionamiento del menú hamburguesa
 var headerNav = document.querySelector('.header__nav');
 var iconMenu = document.querySelector('.icon-menu');
 var handleMenuClick = function () {
@@ -22,13 +14,36 @@ function handleWindowResize () {
 }
 window.addEventListener('resize', handleWindowResize);
 
-/*var btnRight = document.querySelector(".nombredelaclase");
-var sliderStrip = document.querySelector(".slider__strip");
+//Javascript para el funcionamiento del slider de galería de imagenes
+
+var btnRight = document.querySelector('.arrow--right');
+var btnLeft = document.querySelector('.arrow--left');
+var galleryStrip = document.querySelector(".gallery__strip");
+var posX = 0;
+var width = galleryStrip.offsetWidth;
 function handleBtnNextClick(){
-    strip.style.transform = "valor de la cosa en css,siempre en striing comillas";
+    var quantity = galleryStrip.children.length;
+    if(posX > -width * (quantity - 1)){
+        posX -= width;
+    } else {
+        posX = 0;
+    }
+    galleryStrip.style.transform = 'translate(' + posX + 'px, 0px)';
 }
     btnRight.addEventListener('click',handleBtnNextClick);
-*/
+
+    function handleBtnPrevClick () {
+        var quantity = galleryStrip.children.length;
+        if(posX < 0){
+            posX += width;
+        } else {
+            posX = -width * (quantity - 1);
+        }
+        galleryStrip.style.transform = 'translate(' + posX + 'px, 0px)';
+    }
+    btnLeft.addEventListener('click', handleBtnPrevClick);
+
+    
 
 
 
