1669ebd55b3bb69b89f0800fcacbc410b8fc977d
Julieth Salazar <Juli.Salazar18@hotmail.com>
2021-05-15 10:18:21 -0500

    Project.html added

--- script/main.js
+++ script/main.js
@@ -16,47 +16,13 @@ function handleWindowResize () {
 window.addEventListener('resize', handleWindowResize);
 
 
-
-//Javascript para el funcionamiento del slider de galería de imagenes
-
-var btnRight = document.querySelector('.arrow--right');
-var btnLeft = document.querySelector('.arrow--left');
-var galleryStrip = document.querySelector(".gallery__strip");
-var posX = 0;
-var width = galleryStrip.offsetWidth;
-function handleBtnNextClick(){
-    var quantity = galleryStrip.children.length;
-    if(posX > -width * (quantity - 1)){
-        posX -= width;
-    } else {
-        posX = 0;
-    }
-    galleryStrip.style.transform = 'translate(' + posX + 'px, 0px)';
-}
-    btnRight.addEventListener('click',handleBtnNextClick);
-
-    function handleBtnPrevClick () {
-        var quantity = galleryStrip.children.length;
-        if(posX < 0){
-            posX += width;
-        } else {
-            posX = -width * (quantity - 1);
-        }
-        galleryStrip.style.transform = 'translate(' + posX + 'px, 0px)';
-    }
-    btnLeft.addEventListener('click', handleBtnPrevClick);
-
-//Javascript para el funcionamiento de la interacción del usuario con las imagenes
-
-var projectImage = document.querySelector('.project__image');
-var projectInput = document.querySelector('.project__input');
-function handleProjectInput () {
-    var index = projectInput.value;
-    projectImage.setAttribute('src', './src/images/agenda' + index + '.jpg');
-}
-handleProjectInput();
-projectInput.addEventListener('input', handleProjectInput);
-
-
-
-
+var firebaseConfig = {
+    apiKey: "AIzaSyDauoo9J2YUPVOKTLFBPsvWehs50t69Dqw",
+    authDomain: "portfolio-430e6.firebaseapp.com",
+    projectId: "portfolio-430e6",
+    storageBucket: "portfolio-430e6.appspot.com",
+    messagingSenderId: "69053563916",
+    appId: "1:69053563916:web:411278292dc22eddfeda29"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
